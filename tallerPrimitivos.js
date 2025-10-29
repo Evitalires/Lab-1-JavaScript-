58633c7acd2245ee02f7e18aef6e2f2b49441a92
Evitalires <evitalires@gmail.com>
2025-10-29 13:09:46 -0500

    Resolviendo variables y tipos de datos

---
+++ tallerPrimitivos.js
@@ -0,0 +1,13 @@
+// 1. Variables y Tipos de Datos
+
+// Ejercicio 1:
+const nombre = "Jair";
+let edad = "30";
+let esEstudiante = true;
+
+// Ejercicio 2
+let sinValor
+let valorNulo = null;
+console.log("sinValor = ", sinValor);
+console.log("valorNulo = ", valorNulo);
+
