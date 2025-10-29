85aa271d555fb027cc3b1fec45b5f4b0bdedea36
Evitalires <evitalires@gmail.com>
2025-10-29 14:03:04 -0500

    Realizando los ejercicios de condicionales

--- tallerPrimitivos.js
+++ tallerPrimitivos.js
@@ -21,8 +21,30 @@ let edadCalculada = 2025 - nacimiento;
 console.log("Su edad para el 2025 será: ", edadCalculada);
 
 //Ejercicio 2
-numero1 = prompt("Escriba el primer número: " );
-numero2 = prompt("Escriba el segundo número: " );
+numero1 = parseInt(prompt("Escriba el primer número: " ));
+numero2 = parseInt(prompt("Escriba el segundo número: " ));
 suma = numero1 + numero2;
 console.log("La suma de ", numero1, " y ", numero2, "es :", suma);
 
+
+// Condicionales If
+
+//Ejercicio 1
+
+let edadUsuario = parseInt( prompt("Escriba su edad: "));
+if(edadUsuario > 17 ){
+    console.log("Puedes entrar!");
+} else {
+    console.log("Eres menor de edad! No puedes entrar.");
+}
+
+// Ejercicio2
+
+let numeroPedido = parseInt(prompt("Escriba un numero: "));
+if (numeroPedido < 0 ) {
+    console.log("El numero es negativo");
+} else if (numeroPedido === 0) {
+    console.log("El numero es 0");
+} else {
+    console.log("El numero es positivo.");
+}
