9b706e1f24bae4e324b8790a73bbd2e9e068aa95
Evitalires <evitalires@gmail.com>
2025-10-29 13:44:18 -0500

    Ingreso de datos por teclado

--- tallerPrimitivos.js
+++ tallerPrimitivos.js
@@ -11,3 +11,18 @@ let valorNulo = null;
 console.log("sinValor = ", sinValor);
 console.log("valorNulo = ", valorNulo);
 
+// 2 Ingreso de Datos por Teclado
+
+const prompt = require("prompt-sync")();
+
+//  Ejercicio 1
+let nacimiento = prompt("Escribe su año de nacimiento: ");
+let edadCalculada = 2025 - nacimiento;
+console.log("Su edad para el 2025 será: ", edadCalculada);
+
+//Ejercicio 2
+numero1 = prompt("Escriba el primer número: " );
+numero2 = prompt("Escriba el segundo número: " );
+suma = numero1 + numero2;
+console.log("La suma de ", numero1, " y ", numero2, "es :", suma);
+
